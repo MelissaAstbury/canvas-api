353d926792420058d8330a45a722ba0c71343de0
Melissa Astbury <melissaastbury@hotmail.com>
2020-10-26 21:13:32 +0000

    started to practice using fillRect, strokeRect, clearRect, fillText & strokeText. Shapes have all been rendered on the page

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+
+// fillRect
+ctx.fillStyle = 'red';
+ctx.fillRect(20, 20, 150, 100);
+ctx.fillStyle = 'blue';
+ctx.fillRect(200, 20, 150, 100);
+
+// strokeRect
+ctx.lineWidth = 5;
+ctx.strokeStyle = 'green';
+ctx.strokeRect(100, 200, 150, 100);
+
+// clearRect
+ctx.clearRect(25, 25, 100, 60);
+
+// fillText
+ctx.font = '40px Arial';
+ctx.fillStyle = 'orange';
+ctx.fillText('Hello', 400, 50);
+
+// strokeText
+ctx.lineWidth = 1;
+ctx.strokeStyle = 'purple';
+ctx.strokeText('World', 400, 150);
